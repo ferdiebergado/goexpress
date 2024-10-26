abfab749860d9c2f67a0716fa4c68270cd1a4062
ferdie bergado <ferdiebergado@gmail.com>
2024-10-26 11:21:27 +0800

    Revised documentation

--- router.go
+++ router.go
@@ -5,21 +5,36 @@ import (
 	"net/http"
 )
 
-// Router is a custom HTTP router built on top of ServeMux with middleware support.
-// It allows you to register routes and apply both global and route-specific middleware.
+// Router is a custom HTTP router built on top of http.ServeMux with support for global
+// and route-specific middleware. It allows easy route registration for common HTTP methods
+// (GET, POST, PATCH, PUT, DELETE) and provides a flexible middleware chain for request handling.
 type Router struct {
-	mux         *http.ServeMux
-	middlewares []Middleware
+	mux         *http.ServeMux // underlying HTTP request multiplexer
+	middlewares []Middleware   // slice to store global middleware functions
 }
 
 // Middleware defines the signature for middleware functions.
-// A Middleware takes an http.Handler as input and returns a new http.Handler
-// that can wrap additional functionality around the original handler.
+// A Middleware function takes an http.Handler as input and returns a new http.Handler
+// that wraps additional functionality around the original handler.
+//
+// Example:
+//
+//	func logMiddleware(next http.Handler) http.Handler {
+//	    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//	        log.Println("Request received:", r.URL.Path)
+//	        next.ServeHTTP(w, r)
+//	    })
+//	}
 type Middleware func(http.Handler) http.Handler
 
-// NewRouter creates a new instance of Router with an underlying http.ServeMux
-// and an empty slice for middlewares. This Router can be used to register routes
-// and apply middleware to HTTP requests.
+// NewRouter creates and returns a new instance of Router with an initialized
+// http.ServeMux and an empty slice for middlewares.
+//
+// Example usage:
+//
+//	router := NewRouter()
+//	router.Get("/hello", helloHandler) // Register GET route with handler
+//	http.ListenAndServe(":8080", router) // Start server with Router
 func NewRouter() *Router {
 	return &Router{
 		mux:         http.NewServeMux(),
@@ -27,84 +42,169 @@ func NewRouter() *Router {
 	}
 }
 
-// Use adds a middleware to the router's global middleware chain.
-// The middleware will be applied to every request handled by this Router.
+// Use appends a middleware to the router's global middleware chain. Each middleware
+// added with Use will be applied to every request handled by this Router.
+//
+// Parameters:
+//
+//	mw: Middleware function to be applied globally
+//
+// Example usage:
+//
+//	router.Use(logMiddleware)
 func (r *Router) Use(mw Middleware) {
 	r.middlewares = append(r.middlewares, mw)
 }
 
-// wrap applies a series of middlewares to an http.Handler in reverse order.
-// The handler is wrapped by each middleware so that the outermost middleware
-// is the first to handle the request, and the innermost middleware is the last.
+// wrap applies a series of middlewares to an http.Handler in reverse order,
+// so that the first middleware is the outermost wrapper around the handler.
 func (r *Router) wrap(handler http.Handler, middlewares []Middleware) http.Handler {
 	finalHandler := handler
-
 	for i := len(middlewares) - 1; i >= 0; i-- {
 		finalHandler = middlewares[i](finalHandler)
 	}
-
 	return finalHandler
 }
 
-// Handle registers a new route with a specific pattern and handler.
-// It allows optional route-specific middleware to be applied, which will be executed
-// before the router's global middleware. The final handler is wrapped in both the
-// route-specific and global middleware chains.
+// Handle registers a new route with a specific pattern and handler function, applying
+// optional route-specific middleware. The route-specific middleware will wrap around
+// the handler before the global middleware is applied.
+//
+// Parameters:
+//
+//	pattern: URL pattern to match the route (e.g., "GET /path")
+//	handler: http.Handler for handling requests to this route
+//	middlewares: Optional route-specific middleware to apply before global middleware
+//
+// Example usage:
+//
+//	router.Handle("GET /static", staticFileHandler, authMiddleware)
 func (r *Router) Handle(pattern string, handler http.Handler, middlewares ...Middleware) {
-	// Wrap the handler with route-specific middlewares first.
-	finalHandler := r.wrap(handler, middlewares)
-
-	// Wrap with global middlewares.
-	finalHandler = r.wrap(finalHandler, r.middlewares)
-
+	finalHandler := r.wrap(handler, middlewares)       // Wrap with route-specific middleware
+	finalHandler = r.wrap(finalHandler, r.middlewares) // Wrap with global middleware
 	r.mux.Handle(pattern, finalHandler)
 }
 
-// handleMethod registers a route with a specified HTTP method, path, and handler function.
-// It also allows optional middlewares to be applied to this route.
-// The method and path are combined into a pattern, which is used to route the request.
-func (r *Router) handleMethod(method string, path string, handler http.HandlerFunc, middlewares ...Middleware) {
+// handleMethod registers a route with a specified HTTP method and path, applying
+// any optional middlewares to the handler.
+//
+// Parameters:
+//
+//	method: HTTP method (e.g., "GET", "POST")
+//	path: URL path for the route
+//	handler: Handler function for the route
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.handleMethod("GET", "/info", infoHandler)
+func (r *Router) handleMethod(method, path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	pattern := fmt.Sprintf("%s %s", method, path)
 	r.Handle(pattern, handler, middlewares...)
 }
 
-// Get registers a new route that responds to HTTP GET requests for the specified path.
-// It applies the provided handler and any optional middlewares to the route.
+// Get registers a new GET route for the specified path and handler, applying any optional middleware.
+//
+// Parameters:
+//
+//	path: URL path for the GET route
+//	handler: Handler function for GET requests to the specified path
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.Get("/about", aboutHandler, authMiddleware)
 func (r *Router) Get(path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	r.handleMethod("GET", path, handler, middlewares...)
 }
 
-// Post registers a new route that responds to HTTP POST requests for the specified path.
-// It applies the provided handler and any optional middlewares to the route.
+// Post registers a new POST route for the specified path and handler, applying any optional middleware.
+//
+// Parameters:
+//
+//	path: URL path for the POST route
+//	handler: Handler function for POST requests to the specified path
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.Post("/submit", submitHandler, csrfMiddleware)
 func (r *Router) Post(path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	r.handleMethod("POST", path, handler, middlewares...)
 }
 
-// Patch registers a new route that responds to HTTP PATCH requests for the specified path.
-// It applies the provided handler and any optional middlewares to the route.
+// Patch registers a new PATCH route for the specified path and handler, applying any optional middleware.
+//
+// Parameters:
+//
+//	path: URL path for the PATCH route
+//	handler: Handler function for PATCH requests to the specified path
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.Patch("/update", updateHandler, authMiddleware)
 func (r *Router) Patch(path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	r.handleMethod("PATCH", path, handler, middlewares...)
 }
 
-// Put registers a new route that responds to HTTP PUT requests for the specified path.
-// It applies the provided handler and any optional middlewares to the route.
+// Put registers a new PUT route for the specified path and handler, applying any optional middleware.
+//
+// Parameters:
+//
+//	path: URL path for the PUT route
+//	handler: Handler function for PUT requests to the specified path
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.Put("/create", createHandler)
 func (r *Router) Put(path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	r.handleMethod("PUT", path, handler, middlewares...)
 }
 
-// Delete registers a new route that responds to HTTP DELETE requests for the specified path.
-// It applies the provided handler and any optional middlewares to the route.
+// Delete registers a new DELETE route for the specified path and handler, applying any optional middleware.
+//
+// Parameters:
+//
+//	path: URL path for the DELETE route
+//	handler: Handler function for DELETE requests to the specified path
+//	middlewares: Optional middlewares to apply to this specific route
+//
+// Example usage:
+//
+//	router.Delete("/remove", removeHandler, authMiddleware)
 func (r *Router) Delete(path string, handler http.HandlerFunc, middlewares ...Middleware) {
 	r.handleMethod("DELETE", path, handler, middlewares...)
 }
 
-// ServeHTTP allows the Router to satisfy the http.Handler interface.
-// It delegates the actual request handling to the underlying ServeMux
-// after all middlewares have been applied to the registered handlers.
+// ServeHTTP enables the Router to satisfy the http.Handler interface.
+// It delegates actual request handling to the underlying ServeMux after
+// applying the middleware chain to registered handlers.
+//
+// Example usage:
+//
+//	http.ListenAndServe(":8080", router)
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.mux.ServeHTTP(w, req)
 }
 
+// ServeStatic serves static files from the specified local directory path.
+// It registers a GET route to handle requests for static files and serves them
+// relative to the provided path. The function applies an http.StripPrefix
+// to remove the specified path prefix from incoming requests, allowing
+// files to be directly accessed within the directory.
+//
+// Parameters:
+//
+//	path: The local directory path containing the static files to be served.
+//
+// Example:
+//
+//	r.ServeStatic("assets") // Serves files from the "assets" directory at "/assets/"
+//
+// This function constructs a GET route pattern using the specified path
+// and registers it to the router, enabling clients to access static resources.
 func (r *Router) ServeStatic(path string) {
 	fullPath := fmt.Sprintf("/%s/", path)
 	pattern := fmt.Sprintf("GET %s", fullPath)
