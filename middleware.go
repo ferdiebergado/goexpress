cdef0dfa4770bda8f167252c07b5e6c24482cba0
ferdie bergado <ferdiebergado@gmail.com>
2025-11-22 16:57:48 +0800

    refactor: Remove StripTrailingSlash middleware

--- middleware.go
+++ middleware.go
@@ -1,10 +1,6 @@
 package goexpress
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log/slog"
 	"net"
 	"net/http"
@@ -30,25 +26,10 @@ func LogRequest(next http.Handler) http.Handler {
 	})
 }
 
-// StripTrailingSlashes is middleware that removes any trailing slashes from the URL path
-// (except for the root path "/"). If a trailing slash is found, it redirects the request to the
-// URL without the trailing slash using a 301 (Moved Permanently) status code.
-func StripTrailingSlashes(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
-			// Remove the trailing slash and redirect to the new URL.
-			slog.Info("Removing trailing slash and redirecting...")
-			http.Redirect(w, r, strings.TrimSuffix(r.URL.Path, "/"), http.StatusMovedPermanently)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-// RecoverFromPanic is middleware that recovers from panics that occur during the execution
+// RecoverPanic is middleware that recovers from panics that occur during the execution
 // of the handler. If a panic is detected, it logs the error and stack trace, and returns
 // a 500 (Internal Server Error) response to the client.
-func RecoverFromPanic(next http.Handler) http.Handler {
+func RecoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
@@ -83,44 +64,4 @@ func getIPAddress(r *http.Request) string {
 		return r.RemoteAddr
 	}
 	return ip
-}
-
-func ReadRequestBody(req *http.Request) ([]byte, error) {
-	var body []byte
-	if req.Body != nil {
-		bodyBytes, err := io.ReadAll(req.Body)
-		if err != nil {
-			req.Body = io.NopCloser(bytes.NewBuffer(nil))
-			return nil, fmt.Errorf("error reading request body: %w", err)
-		}
-		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-		body = bodyBytes
-	}
-
-	return body, nil
-}
-
-func Mask(data []byte, fieldsToMask []string) []byte {
-	var dataMap map[string]any
-	err := json.Unmarshal(data, &dataMap)
-	if err != nil {
-		slog.Error("failed to unmarshal input", "reason", err)
-		return data
-	}
-
-	for k := range dataMap {
-		for _, f := range fieldsToMask {
-			if k == f {
-				dataMap[k] = "*"
-			}
-		}
-	}
-
-	bytes, err := json.Marshal(dataMap)
-	if err != nil {
-		slog.Error("failed to marshal data", "reason", err)
-		return data
-	}
-
-	return bytes
 }
