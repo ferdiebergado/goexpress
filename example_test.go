bb2bc451b311015ba3b55e98552be74d4ffb571e
ferdie bergado <ferdiebergado@gmail.com>
2025-11-22 19:58:18 +0800

    docs: Provide complete package example

--- example_test.go
+++ example_test.go
@@ -13,8 +13,49 @@ func Example() {
 	router.Use(goexpress.RecoverPanic)
 	router.Use(goexpress.LogRequest)
 
+	router.Static("/static", "./static")
+
 	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
+	})
+
+	authMiddleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if token := r.Header.Get("Authorization"); token == "" {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	checkContentTypeMiddleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if ct := r.Header.Get("Content-Type"); ct != "application/json" {
+				http.Error(w, "Unsupported media", http.StatusUnsupportedMediaType)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	router.Group("/users", func(usersRouter *goexpress.Router) {
+		usersRouter.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("profile of user: " + r.PathValue("id")))
+		})
+		usersRouter.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte("user created"))
+		}, checkContentTypeMiddleware)
+		usersRouter.Group("/posts", func(postsRouter *goexpress.Router) {
+			postsRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("list of posts"))
+			})
+		})
+	}, authMiddleware)
+
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page your are looking for was not found", http.StatusNotFound)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", router))
