04fea32eab3b1f3a1322d6d59b41b1c3b0fc8543
ferdie bergado <ferdiebergado@gmail.com>
2025-11-22 16:58:12 +0800

    test: Remove TestStripTrailingSlashes

--- middleware_test.go
+++ middleware_test.go
@@ -8,34 +8,17 @@ import (
 	"github.com/ferdiebergado/goexpress"
 )
 
-const (
-	errBodyFmt   = "expected body %q, got %q"
-	errStatusFmt = "expected status %d, got %d"
-)
-
-func TestStripTrailingSlashes(t *testing.T) {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-
-	req := httptest.NewRequest(http.MethodGet, "/trailing-slash/", nil)
-	rec := httptest.NewRecorder()
-
-	goexpress.StripTrailingSlashes(handler).ServeHTTP(rec, req)
-	if rec.Code != http.StatusMovedPermanently {
-		t.Errorf(errStatusFmt, http.StatusMovedPermanently, rec.Code)
-	}
-}
+const errStatusFmt = "expected status %d, got %d"
 
 func TestPanicRecovery(t *testing.T) {
 	handler := http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
 		panic("test panic")
 	})
 
-	req := httptest.NewRequest(http.MethodGet, "/panic", nil)
+	req := httptest.NewRequest(http.MethodGet, "/panic", http.NoBody)
 	rec := httptest.NewRecorder()
 
-	goexpress.RecoverFromPanic(handler).ServeHTTP(rec, req)
+	goexpress.RecoverPanic(handler).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusInternalServerError {
 		t.Errorf(errStatusFmt, http.StatusInternalServerError, rec.Code)
